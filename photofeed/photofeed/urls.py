b41e8f5019c008416349ca5b3c3b5b4cbca91b68
Gamer4life <lpuvilla0001@mymail.lausd.net>
2019-10-07 18:05:09 +0000

    Finished

--- photofeed/photofeed/urls.py
+++ photofeed/photofeed/urls.py
@@ -13,13 +13,13 @@
         1. Import the include() function: from django.conf.urls import url, include
         2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
     """
-    from django.conf.urls import url
-    from django.contrib import admin
-    from feed.views import *
+from django.conf.urls import url
+from django.contrib import admin
+from feed.views import *
 
-    urlpatterns = [
-        url(r'^$', index),
-        url(r'^push_feed$', push_feed),
-        url(r'^pusher_authentication', pusher_authentication),
-        url(r'^admin/', admin.site.urls),
-    ]
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^push_feed$', push_feed),
+    url(r'^pusher_authentication', pusher_authentication),
+    url(r'^admin/', admin.site.urls),
+]
